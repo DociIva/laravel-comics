c3cd89eaa754a04605f62e2211210a736475fa3d
DociIva <78482428+DociIva@users.noreply.github.com>
2021-06-08 17:58:42 +0200

    add-header-prima parte

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<!-- HEADER IL CODICE -->
+<header class="header container">
+        <a href="/">  <img src="{{ asset('images/dc-logo.png')}}" alt="Logo"></a>
+        <nav class="nav-header">
+            <ul>
+                <li>
+                    <a href="#" class="text-color">Home</a>
+                </li>
+                <li>
+                    <a href="#" class="active">Comics</a>
+                </li>
+                <li>
+                    <a href="#" class="text-color">News</a>
+                </li>
+            </ul>
+        </nav>
+         
+</header>
